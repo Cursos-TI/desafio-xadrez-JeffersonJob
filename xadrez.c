6d8d18a0b834b2804396f907765c7c9c7b72d621
Jefferson de Souza Dias <soudias09@gmail.com>
2025-05-15 14:37:13 +0000

    desafio novato

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,35 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre = 1;
+    int bispo = 1;
+    int rainha = 1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Torre move: \n");
+    while (torre <= 5)
+    {
+        printf("Direita\n");
+        torre++;
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Bispo move: \n");
+    do
+    {
+        printf("Cima\nEsquerda\n");
+        bispo++;
+    } while (bispo <= 5);
 
+    printf("\n");
+
+    printf("Rainha move: \n");
+    for ( rainha; rainha <= 8; rainha++)
+    {
+        printf("Esquerda\n");
+    }
+    
+    return 0;
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
